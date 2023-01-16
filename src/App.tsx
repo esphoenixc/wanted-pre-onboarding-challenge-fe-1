66ca29e9bb8f76835233be8a139f6956396f1612
phoenix52 <phoenix@tape.ooo>
2023-01-17 00:27:38 +0900

    react-router-dom setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import React, { useState, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = React.lazy(() => import("@/pages/Home"));
+const Login = React.lazy(() => import("@/pages/Login"));
+const NotFound = React.lazy(() => import("@/pages/NotFound"));
+const Header = React.lazy(() => import("@/pages/Header"));
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const user = "Phoenix";
+  // const [count, setCount] = useState(0);
 
   return (
-    <div className=" bg-blue-700">
-      <div className="text-white font-extrabold bg-gray-500 flex justify-center items-center hover:text-green-400 hover:scale-95">
-        {user}
-      </div>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className=" bg-purple-400 text-white text-2xl font-extrabold flex justify-center items-center">
+      Header
+    </div>
+  );
+}
+
+export default Header;
